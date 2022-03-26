dd6302d3c42bcc3dab847922ecb095e06ac89ea8
sethgibb <sethgibson14@gmail.com>
2022-03-26 18:42:11 -0500

    tore it down
    forget it

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import Topbar from './components/topbar/Topbar';
 import Login from './pages/login/Login';
 import Profile from './pages/profile/Profile';
-import Hash from './pages/hashTesting/hashElement';
+
 
 function App() {
   return (
-    <Hash />
+    <Login />
   );
 }
 
